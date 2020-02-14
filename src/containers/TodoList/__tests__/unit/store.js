d04314f5062c91177cead0dc3e37d3dd2346746b
wangeastsea <wangeastsea@icloud.com>
2020-02-14 18:59:56 +0800

    update

--- src/containers/TodoList/__tests__/unit/store.js
+++ src/containers/TodoList/__tests__/unit/store.js
@@ -1,10 +1,7 @@
-
 import store from '@/store'
 
-it(`
- 当store接收changeInputValue 的commit 时， inputValue发生变化
-`, () => {
-  const value = '1234'
+it('当 store 接受 changeInputValue的commit时，inputValue 发生变化', () => {
+  const value = '123'
   store.commit('changeInputValue', value)
   expect(store.state.inputValue).toBe(value)
 })
